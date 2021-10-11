23dca025021349b435caa68d1b0500bbf6fdb352
Marcus Sawyer <75795150+08msawyer@users.noreply.github.com>
2021-10-11 09:15:31 +0000

    Remove dependency on JQuery and tweak guidelines.

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -3,17 +3,26 @@ let expanded = false;
 function expandNavbar() {
     expanded = !expanded
     if (expanded) {
-        $("#navbar").addClass("expanded")
+        document.getElementById("navbar").classList.add("expanded")
     } else {
-        $("#navbar").removeClass("expanded")
+        document.getElementById("navbar").classList.remove("expanded")
     }
 }
 
-$(() => {
-    $("#nav-placeholder").load("/navbar", () => {
-        if (page !== "/") {
-            $("#home-link").attr("href", "../").removeClass("active")
-        }
-        $(`a[href="${page}"]`).addClass("active")
-    })
-})
+(() => {
+    fetch('/navbar')
+        .then(response => {
+            return response.text()
+        })
+        .then(body => {
+            document.getElementById("nav-placeholder").innerHTML = body
+        })
+        .then(() => {
+            if (page !== "/") {
+                let home = document.getElementById("home-link")
+                home.setAttribute("href", "../")
+                home.classList.remove("active")
+            }
+            document.querySelector(`a[href="${page}"]`).classList.add("active")
+        })
+})()
