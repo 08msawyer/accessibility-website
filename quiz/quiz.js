a997f98146373b7d0ed3e085ddee91a6129c614c
08msawyer <08msawyer@brightoncollege.net>
2021-10-30 00:14:30 +0100

    Update quiz questions and functionality

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,13 +1,28 @@
 const questions = [
     {
-        prompt: "What is your favourite colour?",
-        options: ["Red", "Blue", "Green", "Yellow"],
-        correct: "Blue"
+        prompt: "What is the attribute for embedding a description in an image?",
+        options: ["src", "class", "height", "alt", "data-atf", "data-frt"],
+        correct: "alt"
     },
     {
-        prompt: "What is your favourite animal?",
-        options: ["Tiger", "Lion", "Frog", "Possum"],
-        correct: "Frog"
+        prompt: "Is making text bold a suitable way of distinguishing key information?",
+        options: ["Yes", "No"],
+        correct: "Yes"
+    },
+    {
+        prompt: "How should you present numbered text?",
+        options: ["A paragraph", "An ordered list", "An unordered list", "Alt text in an image", "A table"],
+        correct: "An ordered list"
+    },
+    {
+        prompt: "Is it better to specify the font of text using in-line style attributes or defining font for all of one element in a style sheet?",
+        options: ["In-line style", "Style sheet"],
+        correct: "Style sheet"
+    },
+    {
+        prompt: "How does one specify the language of the website?",
+        options: ["lang=\"en\"", "lang=\"eng\"", "lang=\"UTF-8\""],
+        correct: "lang=\"en\""
     }
 ]
 
@@ -39,8 +54,20 @@ function buttonClick() {
 }
 
 function handleDone() {
-    document.getElementById("quiz-content").innerHTML = `<h3>You scored ${score}/${questions.length}!</h3>`
+    document.getElementById("quiz-content").innerHTML = `
+        <h3>You scored ${score}/${questions.length}!</h3>
+        <div id="feedback">
+            <textarea placeholder="Have any feedback about our quiz?"></textarea>
+            <br>
+            <button class="btn-primary" onclick="submitFeedback()">Submit</button>
+        </div>
+    `
     document.getElementById("quiz-button").remove()
+}
+
+function submitFeedback() {
+    console.log("You think we actually read this?")
+    document.getElementById("feedback").innerHTML = `<h4>Thanks for your feedback!</h4>`
 }
 
 function checkAnswer() {
@@ -52,22 +79,26 @@ function checkAnswer() {
 
     if (selected.value === questions[currentQuestion].correct) {
         score++
-        console.log("Correct!")
+        alert("Correct!")
     } else {
-        console.log("Incorrect!")
+        alert("Incorrect!")
     }
     return true
 }
 
 function setQuizContent(question) {
+    let options = ""
+    for (let option of question.options) {
+        options += `
+        <input type="radio" value='${option}' name="answer" id='${option}'>
+        <label for="${option}">${option}</label>
+        <br>
+        `
+    }
     document.getElementById("quiz-content").innerHTML = `
         <h3>${question.prompt}</h3>
         <div id="questions">
-            ${question.options.map(it => `
-             <input type="radio" value="${it}" name="answer" id="${it}">
-             <label for="${it}">${it}</label>
-             <br>
-            `).join('')}
+            ${options}
         </div>
     `
 }
